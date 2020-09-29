9d4df7579748fb8bdae414c0155e46e653ca3750
PooraniR4 <70837619+PooraniR4@users.noreply.github.com>
2020-09-29 14:36:03 +0530

    Add files via upload

---
+++ Assignment/Assignment 16 29-09/Assignment16 29-09.py
@@ -0,0 +1,8 @@
+#1
+with open('myfile.txt',mode='w') as f:
+    f.write("Python is an object oriented programming language")
+f.close()
+#2
+f1=open('myfile1.txt',mode='w')
+f1.write("Python is an object oriented programming language")
+f1.close()
