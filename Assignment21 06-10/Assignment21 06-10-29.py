9359399f816abfacd674ab96b256fbfafda4954a
PooraniR4 <70837619+PooraniR4@users.noreply.github.com>
2020-10-08 15:22:32 +0530

    Delete Assignment21 06-10-29.py

--- Assignment21 06-10/Assignment21 06-10-29.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,render_template
-app=Flask(__name__)                     #create
-@app.route('/')
-def msg():
-    return render_template("Assignment21.html")
-
-if __name__ == "__main__":
-    app.run(debug="true",port=5098)
