eb9f0be31d5a2b99077298b6a423ebf29d6f6835
PooraniR4 <70837619+PooraniR4@users.noreply.github.com>
2020-10-08 20:11:08 +0530

    Add files via upload

---
+++ Assignment/Assignment22 07-10/Assignment22 07-10.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template
+app=Flask(__name__)                     #create
+@app.route('/')
+def msg():
+    return render_template("Assignment21.html")
+
+if __name__ == "__main__":
+    app.run(debug="true",port=5090)      #DIfferent Port
